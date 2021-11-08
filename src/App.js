623466ebfd63d613038aa4111876c321489e68ad
Eric Oleson <moleso587@west-mec.org>
2021-11-08 11:52:47 -0700

    added gallery animations and partial functionality, added filler home page, and tested navigation functionality (which uses clip path animations to reveal other pages)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Home from './pages/home';
 import Gallery from './pages/gallery';
 
 export default function App() {
   return (
     <div>
+      <Home />
       <Gallery />
-      asdf
-    </div>
+      <h1>blank</h1>
+      <h1>blank</h1>
+      <h1>blank</h1>
+      <h1 className='navSquare' id='square4' onClick={() => {
+        document.getElementsByClassName('gallery')[0].classList.add('open')
+      }}>blank</h1>
+    </div >
   )
 }
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import synth from '../images/synth.png';
+import React, { useRef } from 'react';
+import artGall from '../images/artGallerySmall.png';
+import synth from '../images/synthSmall.png';
 
 export default function Gallery() {
+    const imgSrcs = [artGall, synth];
+    const img1 = useRef();
+    const img2 = useRef();
+    const img3 = useRef();
+    const img4 = useRef();
+    const img5 = useRef();
+    const img6 = useRef();
     return (
         <div className='gallery'>
             <div className='img-orbit'>
-                <img src='https://i.stack.imgur.com/mwFzF.png' />
-                <img src='https://i.stack.imgur.com/mwFzF.png' />
-                <img src={synth} />
-                <img src='https://i.stack.imgur.com/mwFzF.png' />
-                <img src='https://i.stack.imgur.com/mwFzF.png' />
-                <img src='https://i.stack.imgur.com/mwFzF.png' className='current' />
+                <img ref={img1} src={imgSrcs[imgSrcs.length - 1]} />
+                <img ref={img2} src={imgSrcs[6 % 1]} />
+                <img ref={img3} src={imgSrcs[6 % 2]} />
+                <img ref={img4} src={imgSrcs[1]} />
+                <img ref={img5} src={imgSrcs[0]} />
+                <img ref={img6} src={imgSrcs[6 % 5]} />
+                <img src={imgSrcs[5 % 1]} className='current' />
             </div>
+            <button onClick={() => {
+                setTimeout(() => {
+                    img6.current.classList.remove('disappear')
+                    img1.current.classList.remove('fade-in');
+                    img2.current.classList.remove('rotate1')
+                    img3.current.classList.remove('rotate2')
+                    img4.current.classList.remove('rotate3')
+                    img5.current.classList.remove('rotate4')
+                }, 1000);
+                img6.current.classList.add('disappear')
+                img1.current.classList.add('fade-in')
+                img2.current.classList.add('rotate1')
+                img3.current.classList.add('rotate2')
+                img4.current.classList.add('rotate3')
+                img5.current.classList.add('rotate4')
+            }}>asdfsdfafa</button>
         </div>
     )
 }
