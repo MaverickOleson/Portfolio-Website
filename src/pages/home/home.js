b92e27eede3e3f7f21bdd15853b4c092297e25a3
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-01-18 15:26:58 -0700

    changed navsquare borders, removed sample text

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,34 +5,7 @@ export default function Home() {
         <div className='home web-page'>
             <main>
                 <h3>My Portfolio</h3>
-                My heart was stolen by a blind pickpocket
-                In the deep city streets in the summer of 2012
-                And I never even saw her face
-                My dreams were shattered like a stained glass window
-                Jesus in Pieces! I believe I threw a brick right through Him
-                But my memory could not be saved
-                It just seems unlikely that it's me who was to blame
-                So I bookmark my DSM 'cause I need to remember my place
-                This is not enough
-                This is not enough to prove it yet
-                No, I need to hit the bottom
-                This is not enough
-                This is not enough to prove it yet
-                No, I need to hit the bottom
-                (Gotta get to the bottom of this)
-                (Gotta get to the bottom of this)
-                (Gotta get to the bottom of this)
-                Take you with me
-                My soul was crushed like a tall boy
-                Underneath the boots on the curb
-                And I'm still picking up my molars
-                And putting them back in my face
-                My name was soiled by a last call spill
-                With a backwash swill and the blackout killed me
-                Sober on impact from a fall from grace
-                Take the road on higher ground
-                And tell me "don't look down! You'll fall and break your back."
-                But that just reminds me how
+
             </main>
         </div>
     )
